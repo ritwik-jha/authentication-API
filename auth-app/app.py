b3ffa1707cb03e22ce2afab3f5243a98488f60b7
Ritwik46 <ritwikjha46@gmailco>
2021-04-22 10:04:57 +0530

    login logout currentuser routes added

--- auth-app/app.py
+++ auth-app/app.py
@@ -1,13 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dlskfjlkjdalskfjklajdflk'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
-class User(db.Model):
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     email = db.Column(db.String(120), nullable=False, unique=True)
@@ -17,18 +25,51 @@ class User(db.Model):
         return ('User(id: {0}, username: {1}, email: {2})'.format(self.id, self.username, self.email))
 
 @app.route('/signup/<username>/<email>/<passwd>')
-def home(username,email,passwd):
+def signup(username,email,passwd):
     name_exist = User.query.filter_by(username=username).first()
     email_exist = User.query.filter_by(email=email).first()
     hashed_passwd = bcrypt.generate_password_hash(passwd).decode('utf-8')
     if name_exist or email_exist:
-        return "Credentials already exist"
+        return "User already exist"
     else:
         user = User(username=username, email=email, passwd=hashed_passwd)
         db.session.add(user)
         db.session.commit()
         return "account created with username {} and email {}".format(username, email)
 
+@app.route('/login/<email>/<password>')
+def login(email,password):
+    '''    if current_user.is_authenticated:
+        current_user_id = current_user.get_id()
+        return 'user ' + str(User.query.filter_by(id=current_user_id).first().username) + ' is logged in, please logout first'
+        else:'''
+    user = User.query.filter_by(email=email).first()
+    if user and bcrypt.check_password_hash(user.passwd, password):
+        login_user(user, remember=True)
+        return 'Login successful ;)'
+    else:
+        return 'No such user !!! please check your credentials'
+
+@app.route('/currentuser')
+def currentuser():
+    current_user_id = current_user.get_id()
+    if User.query.filter_by(id=current_user_id).first():
+        return 'user ' + str(User.query.filter_by(id=current_user_id).first().username) + ' is logged in'
+    else:
+        return 'No user logged in'
+
+@app.route('/logout')
+def logout():
+    if User.query.filter_by(id=current_user.get_id()).first():
+        current_user_name =  User.query.filter_by(id=current_user.get_id()).first().username
+        logout_user()
+        return 'successfully logged out user {}'.format(current_user_name)
+    else:
+        return 'No user logged in'
+
+@app.route('/database')
+def database():
+    return 
 
 if __name__ == '__main__':
     app.run(port=80, debug=True)
